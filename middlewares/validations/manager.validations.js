26d7a529a663c82ba7e998fd909b3f97479810ff
RafikiMazen <rafeek.mazen@gmail.com>
2020-09-26 01:52:56 +0200

    get all managers

--- middlewares/validations/manager.validations.js
+++ middlewares/validations/manager.validations.js
@@ -1,10 +1,7 @@
 const Joi = require('joi')
 
 const validateGetAllManagers = (req, res, next) => {
-  const schema = Joi.object({
-    Page: Joi.number().integer().required().min(0),
-    Limit: Joi.number().integer().required().min(0),
-  })
+  const schema = Joi.object({})
   const isValid = Joi.validate(req.body, schema)
   if (isValid.error) {
     return res.json({
@@ -14,11 +11,7 @@ const validateGetAllManagers = (req, res, next) => {
   return next()
 }
 const validateGetAllMyEmployees = (req, res, next) => {
-  const schema = Joi.object({
-    Page: Joi.number().integer().required().min(0),
-    Limit: Joi.number().integer().required().min(0),
-    managerId: Joi.number().integer().required().min(0),
-  })
+  const schema = Joi.object({})
   const isValid = Joi.validate(req.body, schema)
   if (isValid.error) {
     return res.json({
--- services/managers.service.js
+++ services/managers.service.js
@@ -1,14 +1,16 @@
 const Manager = require('../models/managers.model')
 const Employee = require('../models/employee_profiles.model')
 const EmployeeProfile = require('../models/employee_profiles.model')
+const jwt = require('jsonwebtoken')
+const User = require('../models/users.model')
 const getAllManagers = async (req, res) => {
   try {
-    const page = req.body.Page
-    const limit = req.body.Limit
+    // const page = req.body.Page
+    // const limit = req.body.Limit
     let result
     result = await Manager.findAll({
-      offset: page * limit,
-      limit,
+      // offset: page * limit,
+      // limit,
       order: [
         ['updatedAt', 'DESC'],
         ['id', 'DESC'],
@@ -32,12 +34,28 @@ const getMyEmployees = async (req, res) => {
   try {
     const page = req.body.Page
     const limit = req.body.Limit
-    const thisManagerId = req.body.managerId
+    const usertoken = req.headers.authorization
+    const token = usertoken.split(' ')
+    // console.log(token)
+    const decoded = jwt.verify(token[0], 'secret')
+    console.log(decoded.id)
+    const user = User.findOne({
+      where: [{ id: decoded.id }],
+    })
+    const manager = Manager.findOne({
+      where: { user_id: user.id },
+    })
+    if (!manager) {
+      return res.json({
+        error: 'User is not a manager',
+      })
+    }
+    // const thisManagerId = req.body.managerId
     let result
     result = await EmployeeProfile.findAll({
       offset: page * limit,
       limit,
-      where: { direct_manager: thisManagerId },
+      where: { direct_manager: manager.id },
       order: [
         ['updatedAt', 'DESC'],
         ['id', 'DESC'],

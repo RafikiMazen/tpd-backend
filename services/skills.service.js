9fe6aebb650b5f549d08b005ea0b87bcdf57678b
Antony Ayoub <antonywaseem@gmail.com>
2020-09-27 05:15:14 +0200

    subcategories res name

--- services/skills.service.js
+++ services/skills.service.js
@@ -1,207 +1,207 @@
-const EmployeeProfile = require('../models/employee_profiles.model')
-const EmployeeSkills = require('../models/employee_skills.model')
-const Skill = require('../models/skills.model')
-const jwt = require('jsonwebtoken')
-const ResourceRequestSkill = require('../models/resource_request_skills.model')
-const SkillCatalog = require('../models/skillcatalog.model')
+const EmployeeProfile = require("../models/employee_profiles.model");
+const EmployeeSkills = require("../models/employee_skills.model");
+const Skill = require("../models/skills.model");
+const jwt = require("jsonwebtoken");
+const ResourceRequestSkill = require("../models/resource_request_skills.model");
+const SkillCatalog = require("../models/skillcatalog.model");
 
 const getMySkills = async (req, res) => {
   try {
-    const usertoken = req.headers.authorization
-    const token = usertoken.split(' ')
-    const decoded = jwt.verify(token[0], process.env.JWT_KEY)
-    let result
+    const usertoken = req.headers.authorization;
+    const token = usertoken.split(" ");
+    const decoded = jwt.verify(token[0], process.env.JWT_KEY);
+    let result;
     result = await EmployeeProfile.findAll({
       where: [{ user_id: decoded.id }],
       include: [{ model: EmployeeSkills, include: [{ model: Skill }] }],
-    })
+    });
     if (!result) {
       return res.json({
-        error: 'Employee does not exist',
-      })
+        error: "Employee does not exist",
+      });
     }
 
     return res.json({
       Employee: result,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
-    })
+      error: "Something went wrong",
+    });
   }
-}
+};
 
 const getCategories = async (req, res) => {
   try {
-    let result
-    result = await SkillCatalog.aggregate('category', 'DISTINCT', {
+    let result;
+    result = await SkillCatalog.aggregate("category", "DISTINCT", {
       plain: false,
-    })
-    var array = []
+    });
+    var array = [];
     for (const row of result) {
-      array.push(row.DISTINCT)
+      array.push(row.DISTINCT);
     }
     return res.json({
       Categories: array,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
-    })
+      error: "Something went wrong",
+    });
   }
-}
+};
 const getSubCategories = async (req, res) => {
   try {
-    let result
+    let result;
     result = await SkillCatalog.findAll({
       where: { category: req.body.category },
-      attributes: ['subcategory'],
-    })
-    var array = []
+      attributes: ["subcategory"],
+    });
+    var array = [];
     for (const row of result) {
-      array.push(row.subcategory)
+      array.push(row.subcategory);
     }
     return res.json({
-      Subategories: array,
-    })
+      Subcategories: array,
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
-    })
+      error: "Something went wrong",
+    });
   }
-}
+};
 
 const addEmployeeSkill = async (req, res) => {
   try {
-    const usertoken = req.headers.authorization
-    const token = usertoken.split(' ')
-    const decoded = jwt.verify(token[0], process.env.JWT_KEY)
+    const usertoken = req.headers.authorization;
+    const token = usertoken.split(" ");
+    const decoded = jwt.verify(token[0], process.env.JWT_KEY);
     const employee = await EmployeeProfile.findOne({
       where: { user_id: decoded.id },
-    })
+    });
     if (!employee) {
       return res.json({
-        error: 'Employee Does not exist',
+        error: "Employee Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
-    const employee_id = employee.id
-    const skill_id = req.body.skill_id
+    const employee_id = employee.id;
+    const skill_id = req.body.skill_id;
 
     // const resourceRequest = req.body.ResourceRequest
     const checkRequest = await Skill.findOne({
       id: skill_id,
-    })
+    });
     if (!checkRequest) {
       return res.json({
-        error: 'Skill Does not exist',
+        error: "Skill Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
     const orderCreated = await EmployeeSkills.create({
       employee_id: employee_id,
       skill_id: skill_id,
-    })
+    });
 
     return res.json({
-      msg: 'Skill successfully added to employee',
+      msg: "Skill successfully added to employee",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const editEmployeeSkill = async (req, res) => {
   try {
-    const usertoken = req.headers.authorization
-    const token = usertoken.split(' ')
-    const decoded = jwt.verify(token[0], process.env.JWT_KEY)
+    const usertoken = req.headers.authorization;
+    const token = usertoken.split(" ");
+    const decoded = jwt.verify(token[0], process.env.JWT_KEY);
     const employee = await EmployeeProfile.findOne({
       where: { user_id: decoded.id },
-    })
+    });
     if (!employee) {
       return res.json({
-        error: 'Employee Does not exist',
+        error: "Employee Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
-    const employee_id = employee.id
-    const skill_id = req.body.skill_id
+    const employee_id = employee.id;
+    const skill_id = req.body.skill_id;
 
     // const resourceRequest = req.body.ResourceRequest
     const checkRequest = await Skill.findOne({
       id: skill_id,
-    })
+    });
     if (!checkRequest) {
       return res.json({
-        error: 'Skill Does not exist',
+        error: "Skill Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
     const employeeSkill = await EmployeeSkills.findOne({
       where: { id: req.body.id },
-    })
+    });
     if (!employeeSkill) {
       return res.json({
-        error: 'Employee Skill Does not exist',
+        error: "Employee Skill Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
-    const body = req.body
-    body.employee_id = employee_id
+    const body = req.body;
+    body.employee_id = employee_id;
     const orderCreated = await EmployeeSkills.update(body, {
       where: {
         id: req.body.skill_id,
       },
-    })
+    });
 
     return res.json({
-      msg: 'Employee Skill successfully updated',
+      msg: "Employee Skill successfully updated",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const deleteEmployeeSkill = async (req, res) => {
   try {
-    const usertoken = req.headers.authorization
-    const token = usertoken.split(' ')
-    const decoded = jwt.verify(token[0], process.env.JWT_KEY)
+    const usertoken = req.headers.authorization;
+    const token = usertoken.split(" ");
+    const decoded = jwt.verify(token[0], process.env.JWT_KEY);
     const employee = await EmployeeProfile.findOne({
       where: { user_id: decoded.id },
-    })
+    });
     if (!employee) {
       return res.json({
-        error: 'Employee Does not exist',
+        error: "Employee Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
-    const employee_id = employee.id
+    const employee_id = employee.id;
     const skill = await EmployeeSkills.findOne({
       where: {
         id: req.body.id,
         employee_id: employee_id,
       },
-    })
+    });
     if (!skill) {
       return res.json({
-        error: 'This skill does not exist for this employeed',
+        error: "This skill does not exist for this employeed",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     await EmployeeSkills.destroy({
@@ -209,48 +209,48 @@ const deleteEmployeeSkill = async (req, res) => {
         id: req.body.id,
         employee_id: employee_id,
       },
-    })
+    });
 
     return res.json({
-      msg: 'Employee Skill successfully deleted',
+      msg: "Employee Skill successfully deleted",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const addSkill = async (req, res) => {
   try {
-    const SkillBody = req.body.Skill
+    const SkillBody = req.body.Skill;
     const skill = await Skill.findOne({
       where: { skill_name: SkillBody.skill_name },
-    })
+    });
     if (skill) {
       return res.json({
-        error: 'This skill name already exits',
+        error: "This skill name already exits",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
-    const orderCreated = await Skill.create(SkillBody)
+    const orderCreated = await Skill.create(SkillBody);
 
     return res.json({
-      msg: 'Skill added',
+      msg: "Skill added",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 module.exports = {
   getMySkills,
   getCategories,
@@ -259,4 +259,4 @@ module.exports = {
   editEmployeeSkill,
   deleteEmployeeSkill,
   addSkill,
-}
+};

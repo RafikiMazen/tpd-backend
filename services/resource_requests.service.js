34ba9d1c9379a084c92f8278725427a3df83c9d0
Antony Ayoub <antonywaseem@gmail.com>
2020-09-28 21:04:37 +0200

    added header for export response

--- services/resource_requests.service.js
+++ services/resource_requests.service.js
@@ -1,45 +1,45 @@
-const { filter } = require('compression')
-const ResourceRequestSkill = require('../models/resource_request_skills.model')
-const ResourceRequest = require('../models/resource_requests.model')
-const ResourceRequestAction = require('../models/resource_requests_actions.model')
-const flatten = require('flat').flatten
-const { Parser } = require('json2csv')
-const { releaseRequestStatus } = require('../constants/enums')
+const { filter } = require("compression");
+const ResourceRequestSkill = require("../models/resource_request_skills.model");
+const ResourceRequest = require("../models/resource_requests.model");
+const ResourceRequestAction = require("../models/resource_requests_actions.model");
+const flatten = require("flat").flatten;
+const { Parser } = require("json2csv");
+const { releaseRequestStatus } = require("../constants/enums");
 
 //view resource requests list with fiters
 const getAllResourceRequests = async (req, res) => {
   try {
-    const page = req.body.Page
-    const limit = req.body.Limit
-    const filters = req.body.Filters
-    var filtersMainApplied = []
-    var filtersSecondaryApplied = []
+    const page = req.body.Page;
+    const limit = req.body.Limit;
+    const filters = req.body.Filters;
+    var filtersMainApplied = [];
+    var filtersSecondaryApplied = [];
     if (filters) {
-      const values = Object.values(filters)
+      const values = Object.values(filters);
       Object.keys(filters).forEach((key, index) => {
-        if (key === 'category' || key === 'subcategory') {
+        if (key === "category" || key === "subcategory") {
           filtersSecondaryApplied.push({
             [key]: filters[key],
-          })
+          });
         } else {
           filtersMainApplied.push({
             [key]: filters[key],
-          })
+          });
         }
-      })
+      });
     }
-    let result
+    let result;
     ResourceRequest.hasMany(ResourceRequestSkill, {
-      foreignKey: 'request_reference_number',
-    })
+      foreignKey: "request_reference_number",
+    });
     if (filtersSecondaryApplied.length != 0) {
       result = await ResourceRequest.findAll({
         offset: page * limit,
         limit,
         where: filtersMainApplied,
         order: [
-          ['updatedAt', 'DESC'],
-          ['reference_number', 'DESC'],
+          ["updatedAt", "DESC"],
+          ["reference_number", "DESC"],
         ],
         include: [
           {
@@ -48,62 +48,63 @@ const getAllResourceRequests = async (req, res) => {
             required: true,
           },
         ],
-      })
+      });
     } else {
       result = await ResourceRequest.findAll({
         offset: page * limit,
         limit,
         where: filtersMainApplied,
         order: [
-          ['updatedAt', 'DESC'],
-          ['reference_number', 'DESC'],
+          ["updatedAt", "DESC"],
+          ["reference_number", "DESC"],
         ],
-      })
+      });
     }
-    const count = result.length
+    const count = result.length;
 
     return res.json({
       ResourceRequests: result,
       count,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: unknown
-    })
+    });
   }
-}
+};
 
 const exportAllResourceRequests = async (req, res) => {
+  res.set("Content-Type", "application/octet-stream");
   try {
-    const filters = req.body.Filters
-    var filtersMainApplied = []
-    var filtersSecondaryApplied = []
+    const filters = req.body.Filters;
+    var filtersMainApplied = [];
+    var filtersSecondaryApplied = [];
     if (filters) {
-      const values = Object.values(filters)
+      const values = Object.values(filters);
       Object.keys(filters).forEach((key, index) => {
-        if (key === 'category' || key === 'subcategory') {
+        if (key === "category" || key === "subcategory") {
           filtersSecondaryApplied.push({
             [key]: filters[key],
-          })
+          });
         } else {
           filtersMainApplied.push({
             [key]: filters[key],
-          })
+          });
         }
-      })
+      });
     }
-    let requests
+    let requests;
     ResourceRequest.hasMany(ResourceRequestSkill, {
-      foreignKey: 'request_reference_number',
-    })
+      foreignKey: "request_reference_number",
+    });
     if (filtersSecondaryApplied.length != 0) {
       requests = await ResourceRequest.findAll({
         where: filtersMainApplied,
         order: [
-          ['updatedAt', 'DESC'],
-          ['reference_number', 'DESC'],
+          ["updatedAt", "DESC"],
+          ["reference_number", "DESC"],
         ],
         include: [
           {
@@ -112,163 +113,131 @@ const exportAllResourceRequests = async (req, res) => {
             required: true,
           },
         ],
-      })
+      });
     } else {
       requests = await ResourceRequest.findAll({
         where: filtersMainApplied,
         order: [
-          ['updatedAt', 'DESC'],
-          ['reference_number', 'DESC'],
+          ["updatedAt", "DESC"],
+          ["reference_number", "DESC"],
         ],
-      })
+      });
     }
-    const count = requests.length
+    const count = requests.length;
 
-    const result = JSON.parse(JSON.stringify(requests))
-    var max_length = 0
-    var fields = []
-    var fieldNames = []
+    const result = JSON.parse(JSON.stringify(requests));
+    var max_length = 0;
+    var fields = [];
+    var fieldNames = [];
     for (var i = 0; i < result.length; i++) {
       if (Object.keys(flatten(result[i])).length > max_length) {
-        max_length = Object.keys(flatten(result[i])).length
-        fields = Object.keys(flatten(result[i]))
-        fieldNames = Object.keys(flatten(result[i]))
+        max_length = Object.keys(flatten(result[i])).length;
+        fields = Object.keys(flatten(result[i]));
+        fieldNames = Object.keys(flatten(result[i]));
       }
     }
     const parser = new Parser({
       fields,
       unwind: fieldNames,
-    })
-    const data = parser.parse(result)
-    res.attachment('allReleaseRequests.csv')
-    res.status(200).send(data)
-    return
+    });
+    const data = parser.parse(result);
+    res.attachment("allReleaseRequests.csv");
+    res.status(200).send(data);
+    return;
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: unknown
-    })
+    });
   }
-}
+};
 
 const addResourceRequest = async (req, res) => {
   try {
-    const resourceRequest = req.body.ResourceRequest
+    const resourceRequest = req.body.ResourceRequest;
 
-    const requestCreated = await ResourceRequest.create(resourceRequest)
-    const skills = req.body.Skills
+    const requestCreated = await ResourceRequest.create(resourceRequest);
+    const skills = req.body.Skills;
     if (skills) {
       for (const skill of skills) {
         ResourceRequestSkill.create({
           request_reference_number: requestCreated.id,
           category: skill.category,
           subcategory: skill.subcategory,
-        })
+        });
       }
     }
 
     return res.json({
-      msg: 'Request successfully added',
+      msg: "Request successfully added",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const updateResourceRequest = async (req, res) => {
   try {
-    const resourceRequest = req.body.ResourceRequest
-    const usertoken = req.headers.authorization
-    const token = usertoken.split(' ')
-    const decoded = jwt.verify(token[0], process.env.JWT_KEY)
+    const resourceRequest = req.body.ResourceRequest;
+    const usertoken = req.headers.authorization;
+    const token = usertoken.split(" ");
+    const decoded = jwt.verify(token[0], process.env.JWT_KEY);
     const checkRequest = await ResourceRequest.findOne({
       reference_number: resourceRequest.reference_number,
-    })
+    });
     if (!checkRequest) {
       return res.json({
-        error: 'Request Does not exist',
+        error: "Request Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     if (
       checkRequest.status != releaseRequestStatus.OPEN &&
-      !decoded.roles.includes('TPD Team')
+      !decoded.roles.includes("TPD Team")
     ) {
       return res.json({
-        error: 'Request status is not open',
+        error: "Request status is not open",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     const requestEdited = await ResourceRequest.update(resourceRequest, {
       where: {
         reference_number: resourceRequest.reference_number,
       },
-    })
+    });
 
     return res.json({
-      msg: 'Request successfully updated',
+      msg: "Request successfully updated",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const deleteResourceRequestSkill = async (req, res) => {
   try {
-    const resourceRequestSkill = req.body.ResourceRequestSkill
+    const resourceRequestSkill = req.body.ResourceRequestSkill;
     const checkRequest = await ResourceRequest.findOne({
       reference_number: resourceRequestSkill.request_reference_number,
-    })
+    });
     if (!checkRequest) {
       return res.json({
-        error: 'Request Does not exist',
+        error: "Request Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
-    }
-
-    const requestDeleted = await ResourceRequestSkill.destroy(resourceRequestSkill, {
-      where: {
-        id: resourceRequestSkill.id,
-      },
-    })
-
-    return res.json({
-      msg: 'Request successfully deleted',
-      // statusCode: statusCodes.success,
-    })
-  } catch (exception) {
-    console.log(exception)
-    return res.json({
-      error: 'Something went wrong',
-      // statusCode: statusCodes.unknown,
-    })
-  }
-}
-
-const addResourceRequestSkill = async (req, res) => {
-  try {
-    const resourceRequestSkill = req.body.ResourceRequestSkill
-    const checkRequest = await ResourceRequest.findOne({
-      reference_number: resourceRequestSkill.request_reference_number,
-    })
-    if (!checkRequest) {
-      return res.json({
-        error: 'Request Does not exist',
-        // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     const requestDeleted = await ResourceRequestSkill.destroy(
@@ -278,52 +247,87 @@ const addResourceRequestSkill = async (req, res) => {
           id: resourceRequestSkill.id,
         },
       }
-    )
+    );
 
     return res.json({
-      msg: 'Request successfully deleted',
+      msg: "Request successfully deleted",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
+
+const addResourceRequestSkill = async (req, res) => {
+  try {
+    const resourceRequestSkill = req.body.ResourceRequestSkill;
+    const checkRequest = await ResourceRequest.findOne({
+      reference_number: resourceRequestSkill.request_reference_number,
+    });
+    if (!checkRequest) {
+      return res.json({
+        error: "Request Does not exist",
+        // statusCode: statusCodes.entityNotFound,
+      });
+    }
+
+    const requestDeleted = await ResourceRequestSkill.destroy(
+      resourceRequestSkill,
+      {
+        where: {
+          id: resourceRequestSkill.id,
+        },
+      }
+    );
+
+    return res.json({
+      msg: "Request successfully deleted",
+      // statusCode: statusCodes.success,
+    });
+  } catch (exception) {
+    console.log(exception);
+    return res.json({
+      error: "Something went wrong",
+      // statusCode: statusCodes.unknown,
+    });
+  }
+};
 
 const deleteResourceRequest = async (req, res) => {
   try {
-    const resourceRequest = req.body.ResourceRequest
+    const resourceRequest = req.body.ResourceRequest;
     const checkCustomer = await ResourceRequest.findOne({
       reference_number: resourceRequest.reference_number,
-    })
+    });
     if (!checkCustomer) {
       return res.json({
-        error: 'Request Does not exist',
+        error: "Request Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     const requestDeleted = await ResourceRequest.destroy(resourceRequest, {
       where: {
         reference_number: resourceRequest.reference_number,
       },
-    })
+    });
 
     return res.json({
-      msg: 'Request successfully deleted',
+      msg: "Request successfully deleted",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const getResourceRequest = async (req, res) => {
   try {
@@ -332,67 +336,67 @@ const getResourceRequest = async (req, res) => {
         request_reference_number:
           req.body.ResourceRequest.request_reference_number,
       },
-    })
+    });
     if (!resourceRequest) {
       return res.json({
-        msg: 'Request Not Found',
+        msg: "Request Not Found",
         // statusCode: statusCodes.success,
-      })
+      });
     }
     return res.json({
       ResourceRequest: resourceRequest,
       //  statusCode: success
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 const addResourceRequestِAction = async (req, res) => {
   try {
-    const resourceRequestAction = req.body.ResourceRequestAction
+    const resourceRequestAction = req.body.ResourceRequestAction;
 
     // const resourceRequest = req.body.ResourceRequest
     const checkRequest = await ResourceRequest.findOne({
       request_reference_number: req.body.request_reference_number,
-    })
+    });
     if (!checkRequest) {
       return res.json({
-        error: 'Request Does not exist',
+        error: "Request Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
     const orderCreated = await ResourceRequestAction.create(
       resourceRequestAction
-    )
+    );
 
     return res.json({
-      msg: 'Action successfully added',
+      msg: "Action successfully added",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 const updateResourceRequestAction = async (req, res) => {
   try {
-    const resourceRequestAction = req.body.ResourceRequestAction
+    const resourceRequestAction = req.body.ResourceRequestAction;
     const checkAction = await ResourceRequestAction.findOne({
       action_id: resourceRequestAction.action_id,
-    })
+    });
     if (!checkAction) {
       return res.json({
-        error: 'Action Does not exist',
+        error: "Action Does not exist",
         // statusCode: statusCodes.entityNotFound,
-      })
+      });
     }
 
     const actionEdited = await ResourceRequestAction.update(
@@ -402,46 +406,46 @@ const updateResourceRequestAction = async (req, res) => {
           action_id: resourceRequestAction.action_id,
         },
       }
-    )
+    );
 
     return res.json({
-      msg: 'Action successfully updated',
+      msg: "Action successfully updated",
       // statusCode: statusCodes.success,
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 const getResourceRequestActions = async (req, res) => {
   try {
     const resourceRequest = await ResourceRequest.findOne({
       where: { reference_number: req.body.ResourceRequest.reference_number },
-    })
+    });
     if (!resourceRequest) {
       return res.json({
-        msg: 'Request Not Found',
+        msg: "Request Not Found",
         // statusCode: statusCodes.success,
-      })
+      });
     }
     const resourceRequestActions = await ResourceRequestAction.findAll({
       where: { reference_number: req.body.ResourceRequest.reference_number },
-    })
+    });
     return res.json({
       ResourceRequestActions: resourceRequestActions,
       //  statusCode: success
-    })
+    });
   } catch (exception) {
-    console.log(exception)
+    console.log(exception);
     return res.json({
-      error: 'Something went wrong',
+      error: "Something went wrong",
       // statusCode: statusCodes.unknown,
-    })
+    });
   }
-}
+};
 
 module.exports = {
   getAllResourceRequests,
@@ -453,4 +457,4 @@ module.exports = {
   updateResourceRequestAction,
   getResourceRequestActions,
   deleteResourceRequestSkill,
-}
+};

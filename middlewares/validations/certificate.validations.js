5c83cea1924cd941a2612b133e276fedde5c0597
Antony Ayoub <antonywaseem@gmail.com>
2020-10-01 05:58:31 +0200

    edit certifcate added provider id

--- middlewares/validations/certificate.validations.js
+++ middlewares/validations/certificate.validations.js
@@ -1,46 +1,46 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 const validateAddEmployeeCertificate = (req, res, next) => {
   const schema = Joi.object({
     certificate_name: Joi.string().required(),
     expiry_date: Joi.date().required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateEditEmployeeCertificate = (req, res, next) => {
   const schema = Joi.object({
     certificate_id: Joi.number().integer().required(),
     expiry_date: Joi.date(),
     id: Joi.number().integer().required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateDeleteEmployeeCertificate = (req, res, next) => {
   const schema = Joi.object({
     id: Joi.number().integer().required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateAddCertification = (req, res, next) => {
   const schema = Joi.object({
@@ -48,87 +48,87 @@ const validateAddCertification = (req, res, next) => {
       certification_provider_id: Joi.number().integer().required(),
       certification_name: Joi.string().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 const validateAddCertificationProvider = (req, res, next) => {
   const schema = Joi.object({
     CertificationProvider: Joi.object({
       certification_provider_name: Joi.string().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 const validateEditCertificationProvider = (req, res, next) => {
   const schema = Joi.object({
     CertificationProvider: Joi.object({
       certification_provider_id: Joi.number().integer().required(),
       certification_provider_name: Joi.string().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateDeleteCertificationProvider = (req, res, next) => {
   const schema = Joi.object({
     CertificationProvider: Joi.object({
       certification_provider_id: Joi.number().integer().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateGetCertificationsByProvider = (req, res, next) => {
   const schema = Joi.object({
     certification_provider_id: Joi.number().required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateGetAllCertificates = (req, res, next) => {
   const schema = Joi.object({
     Filters: Joi.object({
       certification_provider_id: Joi.number().integer(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateGetAllCertificatesHistory = (req, res, next) => {
   const schema = Joi.object({
@@ -139,45 +139,46 @@ const validateGetAllCertificatesHistory = (req, res, next) => {
       certification_id: Joi.number().integer(),
       employee_id: Joi.number().integer(),
     }),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 const validateDeleteCertification = (req, res, next) => {
   const schema = Joi.object({
     Certification: Joi.object({
       id: Joi.number().integer().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 const validateEditCertification = (req, res, next) => {
   const schema = Joi.object({
     Certification: Joi.object({
       id: Joi.number().integer().required(),
       certification_name: Joi.string().required(),
+      certification_provider_id: Joi.number().integer().required(),
     }).required(),
-  })
-  const isValid = Joi.validate(req.body, schema)
+  });
+  const isValid = Joi.validate(req.body, schema);
   if (isValid.error) {
     return res.json({
       error: isValid.error.details[0].message,
-    })
+    });
   }
-  return next()
-}
+  return next();
+};
 
 module.exports = {
   validateAddEmployeeCertificate,
@@ -192,4 +193,4 @@ module.exports = {
   validateDeleteCertification,
   validateEditCertification,
   validateGetAllCertificatesHistory,
-}
+};
